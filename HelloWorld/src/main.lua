362a83337acad836aa78fd5e2871a9abde1e5f48
Hellc <caiwb1990@gmail.com>
2015-04-25 19:32:22 +0800

    - add function

--- HelloWorld/src/main.lua
+++ HelloWorld/src/main.lua
@@ -166,9 +166,44 @@ local function main()
   print(string.format("%o, %x ,%X",10,10,10))
   
    
-  print("===================")  
+  print("===================function")
+  
+  function rectangleArea(width, height)
+    local area = width * height
+    return area
+  end
+  print("calc area (20 * 15) : " .. rectangleArea(20,15))
+  
+  --变量作用域
+  local global = 1
+  function f()
+    local local1 = 2
+    global = global +1
+    return global
+  end
+  
+  f()
+  print(global) --2
+  print(local1) --nil
+  
+  --多重返回值
+  function calcRectanle(width,height)
+    local area = width * height
+    local perimeter = (width + height) *2
+    
+    return area,perimeter
+  end
+  print(calcRectanle(20,15))
+    
   print("===================")  
   
   print("===================")  
+  print("===================") 
+  print("===================") 
+  print("===================") 
+  print("===================") 
+  print("===================") 
+  print("===================") 
+  
 end
 main()
