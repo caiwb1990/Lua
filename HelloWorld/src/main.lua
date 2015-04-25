ff33dc6a91ad76d4b90fca0e16f7b51335d306b1
Hellc <caiwb1990@gmail.com>
2015-04-25 13:18:06 +0800

    - update for biao type

--- HelloWorld/src/main.lua
+++ HelloWorld/src/main.lua
@@ -103,7 +103,24 @@ local function main()
   
   --return
     
-  print("===================")  
+  print("===================表类型")
+  --Dict
+  stu1 = {id="100",name="tony",age=19}
+  stu2 = {["id"]="100", ["name"]="tony",["age"]=19}
+  
+  print("std1 id:"..stu1.id)    
+  print("std1 name:"..stu1.name)
+  print("std1 age:"..stu1.age)
+
+  print("std2 id:"..stu2["id"])    
+  print("std2 name:"..stu2["name"])
+  print("std2 age:"..stu2["age"])
+  
+  --for
+  for i,v in ipairs(stu1) do
+    print(k.. ":" .. v)
+  end
+  
   print("===================")  
   print("===================")  
   print("===================")  
