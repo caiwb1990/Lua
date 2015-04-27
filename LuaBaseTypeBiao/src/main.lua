6229e47a7d5816cacc4e917bb50eb09861b488a8
Hellc <caiwb1990@gmail.com>
2015-04-27 18:46:46 +0800

    - lua base type biao

--- LuaBaseTypeBiao/src/main.lua
+++ LuaBaseTypeBiao/src/main.lua
@@ -17,6 +17,22 @@ local function main()
   for k,v in pairs(stu1) do
     print(k.. ":" .. v)
   end
+  
+  print("=============")
+  --通常用;来区分不同类型
+  a = {x = 19,y = 23;"aa","bb","cc"}
+
+  w = {x = 0, y = 0; label = "console"}
+  x = {1,2,3}
+  print(w[1])
+  w[1] ="another filed"
+  print(w[1])
+  x.f = w
+  print(x.f[1])
+  
+  print("=====")
+  polyline = {color="blue",thickness=2,{x=0,y=1},{x = -10,y = 0},{x = 10,y = 1}}
+  print(polyline[2].x)
 
 end
 main()
